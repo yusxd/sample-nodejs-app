25e25f1ae7fb253b1f1294fb69ae4f69f0d216df
Syed Shameer Sarwar <syedshameersarwar@gmail.com>
2021-11-04 16:46:30 +0500

    Adding staging test change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req,res)=>{
- res.send('hello! from aws lambda!');
+ res.send('hello! from aws staging lambda!');
 });
 app.listen(8080);
